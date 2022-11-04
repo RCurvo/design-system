30a6f31961cc4f2a6da03e065482bc037ab5559f
RCurvo <renan.curvo@gmail.com>
2022-11-04 12:53:04 -0300

    adding stitches to react

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,12 @@
+import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+})
+
+export function App() {
+  return <Button>Hello World</Button>
+}
