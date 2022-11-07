010cac549e2a0773bf5f87dfc579a2f55a751e0e
RCurvo <renan.curvo@gmail.com>
2022-11-07 12:58:36 -0300

    Implements Storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,33 @@
-import { colors } from '@ignite-ui/tokens'
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+  },
+
+  '&:hover': {
+    backgroundColor: '$ignite500',
+  },
 })
 
-export function App() {
-  return <Button>Hello World</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
