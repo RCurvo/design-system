6f08685ae5398978b50af852430bfdf306dfbf23
RCurvo <renan.curvo@gmail.com>
2022-11-09 14:03:29 -0300

    Implements Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,18 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/Rcurvo.png',
+    alt: 'Renan Curvo',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
