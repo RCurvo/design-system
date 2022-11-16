80be0ab1771cd49cba429d824e019904fc8cc0f3
RCurvo <renan.curvo@gmail.com>
2022-11-16 09:15:08 -0300

    Implements controls and a11y addon

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quo odio voluptatibus quisquam earum. Consequatur reprehenderit repellat quas deleniti pariatur veniam, voluptatibus recusandae provident. Reprehenderit soluta eveniet ipsa suscipit et.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
