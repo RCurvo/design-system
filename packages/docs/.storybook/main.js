cbe0dff5106ad6b56be5606dad8deee2e9dec6f7
RCurvo <renan.curvo@gmail.com>
2022-11-16 10:07:21 -0300

    fix: Add subpath to Storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, {configType}) => {
+    if (configType === 'PRODUCTION') {
+        config.base = 'design-system/'
+    }
+    return config
   }
 }
